d10af1544761352f19509a78dfc71903bf0af752
Li <2318996718@qq.com>
2023-07-02 11:16:56 +0800

    变更

--- 05 代码版本管理/v1.0/cloud507b1/src/main/java/com/zenq/cloud507b/config/MqttConfig.java
+++ 05 代码版本管理/v1.0/cloud507b1/src/main/java/com/zenq/cloud507b/config/MqttConfig.java
@@ -1,4 +1,11 @@
 package com.zenq.cloud507b.config;
 
 public class MqttConfig {
+    public String broker = "tcp://broker.emqx.io:1883";
+    // TLS/SSL
+    // String broker = "ssl://broker.emqx.io:8883";
+    public String username = "emqx";
+    public String password = "public";
+    public String clientid = "publish_client";
+
 }
